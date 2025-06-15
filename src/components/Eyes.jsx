7c9067a97d8759ca90daa24635ade027b42b7969
“Ayush <ayushkumawat77889@gmail.com>
2025-06-15 15:56:29 +0530

    dd

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+
 
 const Eyes = () => {
+
+  const [rotate,setRotate]=useState(0);
+
+  
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+  
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
+  
+      let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotate(angle - 180);
+    };
+  
+    window.addEventListener("mousemove", handleMouseMove);
+  
+    // ✅ Clean up on unmount
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+  
+
+
+
+
+
+
+
   return (
-    <div>
+    <div className="eyes w-full h-screen overflow-hidden">
+      <div className=" relative flex justify-center items-center w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]">
+
+      <div className="absolute  flex  gap-10   ">
+
+      <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center">
+        <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 flex items-center justify-center">
+
+        <div style={{transform:`  rotate(${rotate}deg)`}}  className="line w-full h-10">
+        <div className="w-10   h-10 rounded-full bg-zinc-100"></div>
+        </div>
+
+       
+        </div>
+
+      </div>
+      <div className="w-[15vw] h-[15vw] rounded-full bg-zinc-100 flex items-center justify-center">
+        <div className="w-2/3 h-2/3 rounded-full bg-zinc-900 flex items-center justify-center">
+
+        <div style={{transform:` rotate(${rotate}deg)`}} className="line w-full h-10">
+        <div className="w-10   h-10 rounded-full bg-zinc-100"></div>
+        </div>
+
+       
+        </div>
+
+      </div>
+      
+
+      </div>
+      </div>
       
     </div>
   )
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -8,12 +8,13 @@ const Marquee = () => {
   return (
     <div className="w-full py-20 bg-[#004d43] rounded-tr-3xl rounded-tl-3xl ">
       <div className="text border-t-2 border-b-2 border-zinc-800 flex whitespace-nowrap overflow-hidden flex gap-10">
-      <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:2}} className="text-[22vw] leading-none font-['Founders_Grotesk_X-Condensed'] font-semibold uppercase -mb-[7vw] pt-10">we are ochi</motion.h1>
-      <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:2}} className="text-[22vw] leading-none font-['Founders_Grotesk_X-Condensed'] font-semibold uppercase -mb-[7vw] pt-10">we are ochi</motion.h1>
-      <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:2}} className="text-[22vw] leading-none font-['Founders_Grotesk_X-Condensed'] font-semibold uppercase -mb-[7vw] pt-10">we are ochi</motion.h1>
+      <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:2}} className="text-[16vw] leading-none font-['Founders_Grotesk_X-Condensed'] font-semibold uppercase -mb-10 pt-10">we are ochi</motion.h1>
+      <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:2}} className="text-[16vw] leading-none font-['Founders_Grotesk_X-Condensed'] font-semibold uppercase -mb-10 pt-10">we are ochi</motion.h1>
+      <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:2}} className="text-[16vw] leading-none font-['Founders_Grotesk_X-Condensed'] font-semibold uppercase -mb-10 pt-10">we are ochi</motion.h1>
+        
       </div>
     </div>
   )
 }
 
-export default Marquee;   
+export default Marquee;  
